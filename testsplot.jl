25e18e074cedb34c1e6bc3876381a94b34bd6c97
Claveg <gabriel@example.fr>
2021-05-26 10:28:34 +0200

    essais sur vraies sequecnes

---
+++ testsplot.jl
@@ -0,0 +1,39 @@
+using Plots
+
+# 10 data points in 4 series
+xs = range(0, 2π, length = 10)
+data = [sin.(xs) cos.(xs) 2sin.(xs) 2cos.(xs)]
+
+# We put labels in a row vector: applies to each series
+labels = ["Apples" "Oranges" "Hats" "Shoes"]
+
+# Marker shapes in a column vector: applies to data points
+markershapes = [:circle, :star5]
+
+# Marker colors in a matrix: applies to series and data points
+markercolors = [
+    :green :orange :black :purple
+    :red   :yellow :brown :white
+]
+
+plot(
+    xs,
+    data,
+    label = labels,
+    shape = markershapes,
+    color = markercolors,
+    markersize = 10
+)
+
+P2 = Plots.P2
+
+gr()
+
+y = rand(10)
+scatter(y,annotations=(3,y[3],text("this is #3",:left)),leg=false)
+
+plotly()
+
+name = ["this is $(i)" for i in 1:10]
+
+scatter(y, hover = name)
